6e0cf8a787a5a07f1561d5df77fb339773a8237c
Ruslan Bilalov <47795314+fradaaa@users.noreply.github.com>
2023-10-16 19:41:35 +0800

    Fix A11y

--- components/movie/Poster/PlayTrailer.tsx
+++ components/movie/Poster/PlayTrailer.tsx
@@ -2,9 +2,13 @@ import { LuPlay } from "react-icons/lu";
 
 const PlayTrailer = () => {
   return (
-    <div className="absolute -right-9 bottom-20 cursor-pointer rounded-sm bg-sky-600 p-4 text-4xl text-white">
-      <LuPlay />
-    </div>
+    <button
+      type="button"
+      className="absolute -right-9 bottom-20 cursor-pointer rounded-sm bg-sky-600 p-4 text-4xl text-white"
+    >
+      <LuPlay aria-hidden="true" />
+      <p className="sr-only">Play trailer</p>
+    </button>
   );
 };
 
