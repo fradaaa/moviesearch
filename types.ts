ef30a76c0e58060091ea50c29a3ab6ab37397589
Ruslan Bilalov <47795314+fradaaa@users.noreply.github.com>
2023-10-16 19:42:42 +0800

    Add Types For Search

--- types.ts
+++ types.ts
@@ -82,3 +82,65 @@ export type Movie = {
     crew: CrewCredit[];
   };
 };
+
+export type MovieSearchResult = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: Date;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+export type TvSearchResult = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  first_air_date: Date;
+  name: string;
+  vote_average: number;
+  vote_count: number;
+};
+
+type KnowFor =
+  | (TvSearchResult & { media_type: "tv" })
+  | (MovieSearchResult & {
+      media_type: "movie";
+    });
+
+export type PersonSearchResult = {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+  known_for: KnowFor[];
+};
+
+export type SearchData =
+  | {
+      type: "movie";
+      items: MovieSearchResult[];
+    }
+  | {
+      type: "person";
+      items: PersonSearchResult[];
+    };
