a428544893b5668fed277ff9fe65fff02fa8f357
Ruslan Bilalov <47795314+fradaaa@users.noreply.github.com>
2023-10-16 19:42:16 +0800

    Update API Token

--- api.ts
+++ api.ts
@@ -3,7 +3,7 @@ import { Movie } from "./types";
 const getData = async <T>(url: string) => {
   const res = await fetch(url, {
     headers: {
-      Authorization: `Bearer ${process.env.TMDB_TOKEN}`,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_TOKEN}`,
     },
   });
   const data = (await res.json()) as T;
