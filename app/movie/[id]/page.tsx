8f099e6590411d864d1ae2102f3b0e5d2b2d8ce1
Ruslan Bilalov <47795314+fradaaa@users.noreply.github.com>
2023-10-17 23:47:50 +0800

    Update Movie Route & It's Metadata

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,13 +1,45 @@
+import { getMovie, getMovieImages } from "@/api";
+import MovieDetails from "@/components/movie/MovieDetails/MovieDetails";
 import MovieInfo from "@/components/movie/MovieInfo/MovieInfo";
 import Poster from "@/components/movie/Poster/Poster";
+import { formatDate } from "@/utils/formatDate";
+import { Metadata, ResolvingMetadata } from "next";
 
-export default function MoviePage({ params }: { params: { id: string } }) {
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const id = params.id;
+
+  const { title, release_date } = await getMovie(id);
+
+  const { year } = formatDate(release_date);
+
+  return {
+    title: `${title}, ${year} - description, cast, images - Moviesearch`,
+  };
+}
+
+export default async function MoviePage({ params }: Props) {
   const { id } = params;
+
+  const {
+    credits: { cast },
+  } = await getMovie(id);
+  const images = await getMovieImages(id);
 
   return (
     <>
-      <Poster id={id} />
-      <MovieInfo id={id} />
+      <div className="flex">
+        <Poster id={id} />
+        <MovieInfo id={id} />
+      </div>
+      <MovieDetails images={images} cast={cast} />
     </>
   );
 }
