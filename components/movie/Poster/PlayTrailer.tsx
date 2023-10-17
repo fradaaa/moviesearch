3b651418f6c5a11511cf91247ead5a8e3ff34cbb
Ruslan Bilalov <47795314+fradaaa@users.noreply.github.com>
2023-10-17 18:19:14 +0800

    Add Modal To Play Trailer Button

--- components/movie/Poster/PlayTrailer.tsx
+++ components/movie/Poster/PlayTrailer.tsx
@@ -1,14 +1,55 @@
-import { LuPlay } from "react-icons/lu";
+"use client";
 
-const PlayTrailer = () => {
+import { MovieInfo, VideoResult } from "@/types";
+import { useEffect, useState } from "react";
+import { LuPlay } from "react-icons/lu";
+import ReactModal from "react-modal";
+import PlayerModal from "../PlayerModal";
+
+type PlayerModalProps = {
+  videos: VideoResult[];
+  movieInfo: MovieInfo;
+};
+
+const PlayTrailer = ({ videos, movieInfo }: PlayerModalProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    ReactModal.setAppElement("body");
+  }, []);
+
+  function openModal() {
+    document.body.style.overflow = "hidden";
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    document.body.style.overflow = "";
+    setIsOpen(false);
+  }
+
   return (
-    <button
-      type="button"
-      className="absolute -right-9 bottom-20 cursor-pointer rounded-sm bg-sky-600 p-4 text-4xl text-white"
-    >
-      <LuPlay aria-hidden="true" />
-      <p className="sr-only">Play trailer</p>
-    </button>
+    <>
+      <button
+        type="button"
+        className="absolute -right-9 bottom-20 cursor-pointer rounded-sm bg-sky-600 p-4 text-4xl text-white"
+        onClick={openModal}
+      >
+        <LuPlay aria-hidden="true" />
+        <p className="sr-only">Play trailer</p>
+      </button>
+      <ReactModal
+        className="h-5/6 w-10/12 overflow-hidden rounded-lg bg-gray-900"
+        overlayClassName="fixed w-full h-screen inset-0 z-[1000] flex justify-center items-center bg-black/80"
+        contentLabel="Play Trailer Modal"
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        shouldCloseOnEsc
+        shouldCloseOnOverlayClick
+      >
+        <PlayerModal videos={videos} movieInfo={movieInfo} />
+      </ReactModal>
+    </>
   );
 };
 
--- components/movie/Poster/Poster.tsx
+++ components/movie/Poster/Poster.tsx
@@ -3,20 +3,39 @@ import { getImageURL } from "@/utils/getImageURL";
 import Image from "next/image";
 import PlayTrailer from "./PlayTrailer";
 import ReleaseDate from "./ReleaseDate";
+import { MovieInfo } from "@/types";
 
 type PosterProps = {
   id: string;
 };
 
 const Poster = async ({ id }: PosterProps) => {
-  const { title, poster_path, release_date } = await getMovie(id);
+  const {
+    title,
+    original_title,
+    poster_path,
+    release_date,
+    genres,
+    vote_average,
+    vote_count,
+    videos: { results },
+  } = await getMovie(id);
   const src = getImageURL.getPoster(poster_path, "original");
+  const movieInfo: MovieInfo = {
+    poster_path,
+    release_date,
+    title,
+    original_title,
+    genres,
+    vote_average,
+    vote_count,
+  };
 
   return (
     <div className="relative basis-2/5 rounded-sm border-8 border-sky-800">
       <Image src={src} alt={`${title}'s poster`} width={500} height={750} />
       <ReleaseDate release_date={release_date} />
-      <PlayTrailer />
+      <PlayTrailer videos={results} movieInfo={movieInfo} />
     </div>
   );
 };
