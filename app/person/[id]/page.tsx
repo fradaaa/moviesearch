bd4b110a34324fccaa830b4ebf4552d7a2d9f174
Ruslan Bilalov <47795314+fradaaa@users.noreply.github.com>
2023-10-19 22:41:51 +0800

    Update Person Route

--- app/person/[id]/page.tsx
+++ app/person/[id]/page.tsx
@@ -1,8 +1,7 @@
-import { getPerson } from "@/api";
+import { getPerson, getPersonCombinedCredits } from "@/api";
 import PersonBiography from "@/components/person/PersonBiography";
 import PersonCredits from "@/components/person/PersonCredits";
 import PersonalInfo from "@/components/person/PersonalInfo";
-import { getImageURL } from "@/utils/getImageURL";
 import { Metadata, ResolvingMetadata } from "next";
 
 type Props = {
@@ -26,16 +25,19 @@ export async function generateMetadata(
 export default async function PersonPage({ params }: Props) {
   const { id } = params;
 
-  const { profile_path, name } = await getPerson(id);
-
-  const src = getImageURL.getProfile(profile_path, "h632");
+  const { known_for_department } = await getPerson(id);
+  const credits = await getPersonCombinedCredits(id);
 
   return (
     <div className="flex gap-8">
       <PersonalInfo id={id} />
       <div className="max-w-[860px] basis-4/5">
         <PersonBiography id={id} />
-        <PersonCredits id={id} />
+        <PersonCredits
+          id={id}
+          known_for_department={known_for_department}
+          credits={credits}
+        />
       </div>
     </div>
   );
